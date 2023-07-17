c50375cc24c09c86b9db49c1ec2a8916546755bf
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-17 19:16:29 +0600

    add trusted partners

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Hero from '@/components/Hero'
 import Product from '@/components/Product'
 import Team from '@/components/Team'
 import Company from '@/components/Company'
+import ReadyToTalk from '@/components/ReadyToTalk'
+import TrustedPartners from '@/components/TrustedPartners'
 
 export default function Home() {
   return (
@@ -10,7 +12,8 @@ export default function Home() {
       <Product />
       <Team />
       <Company />
-
+      <TrustedPartners />
+      <ReadyToTalk />
     </main>
   )
 }
