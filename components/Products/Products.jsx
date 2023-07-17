f32e7e50597fa836ebdd77f4a27fa5533171bfb6
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-17 23:04:55 +0600

    view functionality add

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
 import ProductsSidebar from "../Others/Sidebars/ProductsSidebar";
 import useProducts from "../Hooks/useProducts";
 import Loading from "../Others/Loading";
@@ -12,6 +13,7 @@ const ProductsComp = () => {
     const [reviewFilter, setReviewFilter] = useState([]);
     const [price, setPrice] = useState(100);
     const [priceFilter, setPriceFilter] = useState([]);
+    const [viewBy, setViewBy] = useState(false);
 
     const Products = products?.data?.result;
 
@@ -61,26 +63,26 @@ const ProductsComp = () => {
         setCategoryProduct(result);
     };
 
-    console.log("categoryProduct",categoryProduct);
+    console.log("categoryProduct", categoryProduct);
 
-  /* ----------------------------------------------------------------*/
-  /*                 PRICE RANGE FILTERING FUNCTIONALITY             */
-  /* ----------------------------------------------------------------*/
-  const handlePrice = (num) => {
-    setPrice(num);
-  };
+    /* ----------------------------------------------------------------*/
+    /*                 PRICE RANGE FILTERING FUNCTIONALITY             */
+    /* ----------------------------------------------------------------*/
+    const handlePrice = (num) => {
+        setPrice(num);
+    };
 
-  useEffect(() => {
-    if (price > 5) {
-      const filterPrice = Products?.filter(
-        (pd) => pd.price < parseInt(price, 10)
-      );
-      setSearch([]);
-      setCategoryProduct([]);
-      setReviewFilter([]);
-      setPriceFilter(filterPrice);
-    }
-  }, [price]);
+    useEffect(() => {
+        if (price > 5) {
+            const filterPrice = Products?.filter(
+                (pd) => pd.price < parseInt(price, 10)
+            );
+            setSearch([]);
+            setCategoryProduct([]);
+            setReviewFilter([]);
+            setPriceFilter(filterPrice);
+        }
+    }, [price]);
 
     /* ----------------------------------------------------------------*/
     /*                       Filter By Ratings                         */
@@ -95,6 +97,20 @@ const ProductsComp = () => {
         }
     };
 
+
+    /* ----------------------------------------------------------------*/
+    /*                        Handle View Products                     */
+    /* ----------------------------------------------------------------*/
+    const handleViewBy = (e) => {
+        const view = e.target.value;
+
+        if (view === "list") {
+            setViewBy(true)
+        } else {
+            setViewBy(false)
+        };
+
+    };
 
 
     // Load Products By Filter Type
@@ -118,11 +134,20 @@ const ProductsComp = () => {
 
     return (
         <div className="w-full">
-            <div className="py-3">
+            <div className="py-3 relative">
                 <h3 className="text-4xl text-gray-700 font-bold text-center py-4">
                     User Interface Products
                 </h3>
                 <p className="text-center text-gray-500 text-xl">Building the Future</p>
+
+                <div className="flex justify-between items-center gap-2 px-7 absolute top-5 right-0">
+                    <h3 className="text-gray-700 font-bold">View By:</h3>
+                    <select onChange={handleViewBy} className="select bg-gray-300 text-gray-500 max-w-xs">
+                        <option value="grid">Grid View</option>
+                        <option value="list">List View</option>
+
+                    </select>
+                </div>
             </div>
             <div className="">
                 <div className="bg-base-100">
@@ -140,30 +165,47 @@ const ProductsComp = () => {
                             <div className="col-span-3">
                                 <div className="flex flex-col md:flex-row justify-between items-center gap-2 py-5">
                                     <h3 className="text-gray-400 font-bold">Showing <span className="text-gray-500">{loadProducts?.length}</span> Total Results.</h3>
-                                    <div className="flex justify-between items-center gap-2 px-7">
-                                        <h3 className="text-gray-700 font-bold">Sort By:</h3>
-                                        <select onChange={filterByCategory} className="select bg-gray-300 text-gray-500 max-w-xs">
-                                            <option disabled selected>Select Course</option>
-                                            <option>All Products</option>
-                                            <option>Men's Sneaker</option>
-                                            <option>Men's Pants</option>
-                                            <option>Men's T-Shirt</option>
-                                            <option>Men's Boot</option>
-                                            <option value="Bag">Men's Bags</option>
-                                            {/* <option>Others</option> */}
-                                        </select>
+                                    <div className="flex flex-col justify-between items-center gap-2 px-7">
+                                        <div className="flex justify-between items-center gap-2 px-7">
+                                            <h3 className="text-gray-700 font-bold">Sort By:</h3>
+                                            <select onChange={filterByCategory} className="select bg-gray-300 text-gray-500 max-w-xs">
+                                                <option disabled selected>Select Category</option>
+                                                <option>All Products</option>
+                                                <option>Men's Sneaker</option>
+                                                <option>Men's Pants</option>
+                                                <option>Men's T-Shirt</option>
+                                                <option>Men's Boot</option>
+                                                <option value="Bag">Men's Bags</option>
+                                                {/* <option>Others</option> */}
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="w-full pt-7 grid grid-cols-1 gap-6 sm:grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3 mx-auto">
-                                    {loadProducts?.length ?
-                                        loadProducts?.map((data, index) => (
-                                            <ProductsGrid product={data} key={index} />
-                                        )) :
-                                        <div className="w-screen -ml-28 lg:-ml-44 -mt-20 flex justify-center mx-auto">
-                                            <Loading />
+                                {
+                                    viewBy ?
+                                        <div className="w-full pt-7 grid grid-cols-1 gap-6 justify-center mx-auto">
+                                            {loadProducts?.length ?
+                                                loadProducts?.map((data, index) => (
+                                                    <ProductsList product={data} key={index} />
+                                                )) :
+                                                <div className="w-screen -ml-28 lg:-ml-44 -mt-20 flex justify-center mx-auto">
+                                                    <Loading />
+                                                </div>
+                                            }
+                                        </div> :
+                                        <div className="w-full pt-7 grid grid-cols-1 gap-6 sm:grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3 mx-auto">
+                                            {loadProducts?.length ?
+                                                loadProducts?.map((data, index) => (
+                                                    <ProductsGrid product={data} key={index} />
+                                                )) :
+                                                <div className="w-screen -ml-28 lg:-ml-44 -mt-20 flex justify-center mx-auto">
+                                                    <Loading />
+                                                </div>
+                                            }
                                         </div>
-                                    }
-                                </div>
+                                }
+
+
                             </div>
                         </div>
                     </div>
---
+++ components/Products/ProductsList.jsx
@@ -0,0 +1,159 @@
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const ProductsList = ({ product }) => {
+    // console.log(product);
+    const { _id, name, images, price, ratings, stock, seller, category, shipping, sale } = product;
+    const { push } = useRouter();
+    return (
+        <div onClick={() => push(`/products/${_id}`)} className="mb-4 h-52 flex border cursor-pointer hover:shadow-md hover:-translate-y-3 duration-150 scroll-smooth rounded-xl">
+
+            <div className="h-52 w-1/3 relative overflow-hidden rounded-xl">
+                <img className="h-full w-full relative rounded-xl hover:scale-110 duration-200" src={images[0]} alt="Shoes" />
+                {sale && <img className="w-20 absolute top-0 right-0" src="https://i.ibb.co/5jWHjdt/hot-sale.png" alt="sale" />}
+                {sale && <div className="w-16 h-16 bg-red-700 text-white rounded-full absolute top-0 left-0"><h3 className="font-bold text-2xl text-center font-serif pt-1">{sale}%</h3> <p className="font-semibold text-md text-center font-serif">Off</p></div>}
+                <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out mix-blend-overlay bg-indigo-700"></div>
+            </div>
+            <div className="card-body w-2/3 p-0 relative">
+                <p className="text-xl p-4 font-header text-gray-700 h-10">{name}</p>
+
+                <div className="flex items-center justify-between gap-2 px-4 py-2">
+                    <div className="flex gap-2 items-center">
+                        <h3 className="text-md font-semibold text-gray-600">{ratings}</h3>
+                        {
+
+                            ratings === 1 &&
+                            <div className="rating flex items-center">
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+
+                            </div>
+
+                        }
+                        {
+
+                            ratings === 2 &&
+                            <div className="rating flex items-center">
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+
+                        }
+                        {
+
+                            ratings === 3 &&
+                            <div className="rating flex items-center">
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+
+                        }
+                        {
+
+                            ratings === 4 &&
+                            <div className="rating flex items-center">
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+
+                        }
+                        {
+
+                            ratings === 5 &&
+                            <div className="rating flex items-center">
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                                <svg class="w-4 h-4 text-yellow-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                </svg>
+                            </div>
+
+                        }
+                    </div>
+                    <h3 className="text-gray-600">({stock})</h3>
+
+                </div>
+                <div className="px-4 text-md space-y-1 py-1">
+                    <p className="text-md font-header text-gray-600">Seller: {seller}</p>
+                    {/* <p className="text-md font-header">Category: {category}</p> */}
+                    {/* <p className="text-md font-header">Shipping: ${shipping}</p> */}
+
+                    <div className="py-2">
+                        {
+                            sale ? <h3 className="text-gray-500 text-3xl font-bold"> <span className="text-xl">Price:</span> <span className="line-through">${price}</span> <span className="font-light text-gray-500">/</span><sub className="text-gray-500">${(price - (price * sale / 100)).toFixed(2)}</sub></h3> :
+                                <h3 className="text-gray-500 text-3xl font-bold"> <span className="text-xl">Price:</span> ${price}</h3>
+                        }
+                    </div>
+                </div>
+                <div className="w-40 absolute right-3 bottom-3 flex justify-end text-white items-center gap-2">
+                    <button className="bg-orange-500 hover:bg-orange-600 rounded-xl py-2 text-center font-bold tracking-wide w-full">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductsList;
